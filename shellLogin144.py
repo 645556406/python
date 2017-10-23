d63767e39d635dbe788d5b7a39b598119f5dc864
jiangjun <wangye@onehome.me>
2017-10-24 00:44:43 +0800

    python爬虫脚本

---
+++ shellLogin144.py
@@ -0,0 +1,15 @@
+import requests
+import re
+import urllib
+import random
+from time import sleep
+
+#爬百度百科
+def main():
+    url="https://baike.baidu.com"
+    header={}
+    i=1
+
+if __name__ == '__main__':
+    main()
+
