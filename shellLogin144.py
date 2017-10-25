74abc9151d5326bcefbd1bc1e992f1a19a988933
jiangjun <wangye@onehome.me>
2017-10-25 14:00:16 +0800

    url地址修改

--- shellLogin144.py
+++ shellLogin144.py
@@ -1,16 +1,20 @@
-import requests
-import re
-import urllib
-import random
-from time import sleep
+from urllib import request
+import os
 
-#爬百度百科
+#爬百度
 def main():
-    url="http://www.baidu.com"
-    data = urllib.request.urlopen(url).read()
-    data = data.decode('UTF-8')
+    url="http://www.baidu.com/"
+    data = request.urlopen(url).read()
+    #data = data.decode('UTF-8')
+    data=str(data)
+    print(type(data))
     print("%s",data)
+    wBaiDu=open('1.txt','a+')
+    wBaiDu.write(data+'\n')
+    wBaiDu.close()
 
 if __name__ == '__main__':
     main()
+
+
 
